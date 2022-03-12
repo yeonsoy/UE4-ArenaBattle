1c5edfed0978c2c94ea6f134ced29d0b85c79a52
SeungYeon <yeondiary@gmail.com>
2022-03-12 21:24:26 +0900

    캐릭터 콜리전 설정

--- Source/ArenaBattle/ABCharacter.cpp
+++ Source/ArenaBattle/ABCharacter.cpp
@@ -42,6 +42,8 @@ AABCharacter::AABCharacter()
 
 	MaxCombo = 4;
 	AttackEndComboState();
+
+	GetCapsuleComponent()->SetCollisionProfileName(TEXT("ABCharacter"));
 }
 
 // Called when the game starts or when spawned
