588a3414d93cd8c98bda64ffc6d97ba1365ba798
SeungYeon <yeondiary@gmail.com>
2022-04-13 23:36:32 +0900

    무한 맵의 생성

--- Source/ArenaBattle/Private/ABSection.cpp
+++ Source/ArenaBattle/Private/ABSection.cpp
@@ -22,12 +22,20 @@ AABSection::AABSection()
 		ABLOG(Error, TEXT("Failed to load staticmesh asset. : %s"), *AssetPath);
 	}
 
+	Trigger->OnComponentBeginOverlap.AddDynamic(this, &AABSection::OnTriggerBeginOverlap);
+
 	FString GateAssetPath = TEXT("/Game/Book/StaticMesh/SM_GATE.SM_GATE");
 	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_GATE(*GateAssetPath);
 	if (!SM_GATE.Succeeded())
 	{
 		ABLOG(Error, TEXT("Failed to load staticmesh asset. : %s"), *GateAssetPath);
 	}
+
+	Trigger = CreateDefaultSubobject<UBoxComponent>(TEXT("TRIGGER"));
+	Trigger->SetBoxExtent(FVector(775.0f, 775.0f, 300.0f));
+	Trigger->SetupAttachment(RootComponent);
+	Trigger->SetRelativeLocation(FVector(0.0f, 0.0f, 250.0f));
+	Trigger->SetCollisionProfileName(TEXT("ABTrigger"));
 
 	static FName GateSockets[] = { { TEXT("+XGate") }, { TEXT("-XGate") }, { TEXT("+YGate") }, { TEXT("-YGate") } };
 	for (FName GateSocket : GateSockets)
@@ -38,7 +46,25 @@ AABSection::AABSection()
 		NewGate->SetupAttachment(RootComponent, GateSocket);
 		NewGate->SetRelativeLocation(FVector(0.0f, -80.5f, 0.0f));
 		GateMeshes.Add(NewGate);
+
+		UBoxComponent* NewGateTrigger = CreateDefaultSubobject<UBoxComponent>(*GateSocket.ToString().Append(TEXT("Trigger")));
+		NewGateTrigger->SetBoxExtent(FVector(100.0f, 100.0f, 300.0f));
+		NewGateTrigger->SetupAttachment(RootComponent, GateSocket);
+		NewGateTrigger->SetRelativeLocation(FVector(70.0f, 0.0f, 250.0f));
+		NewGateTrigger->SetCollisionProfileName(TEXT("ABTrigger"));
+		GateTriggers.Add(NewGateTrigger);
+
+		NewGateTrigger->OnComponentBeginOverlap.AddDynamic(this, &AABSection::OnGateTriggerBeginOverlap);
+		NewGateTrigger->ComponentTags.Add(GateSocket);
 	}
+
+	bNoBattle = false;
+}
+
+void AABSection::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+	SetState(bNoBattle ? ESectionState::COMPLETE : ESectionState::READY);
 }
 
 // Called when the game starts or when spawned
@@ -46,4 +72,96 @@ void AABSection::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	SetState(bNoBattle ? ESectionState::COMPLETE : ESectionState::READY);
+}
+
+void AABSection::SetState(ESectionState NewState)
+{
+	switch (NewState)
+	{
+	case ESectionState::READY:
+	{
+		Trigger->SetCollisionProfileName(TEXT("ABTrigger"));
+		for (UBoxComponent* GateTrigger : GateTriggers)
+		{
+			GateTrigger->SetCollisionProfileName(TEXT("NoCollision"));
+		}
+
+		OperateGates(true);
+		break;
+	}
+	case ESectionState::BATTLE:
+	{
+		Trigger->SetCollisionProfileName(TEXT("NoCollision"));
+		for (UBoxComponent* GateTrigger : GateTriggers)
+		{
+			GateTrigger->SetCollisionProfileName(TEXT("NoCollision"));
+		}
+
+		OperateGates(false);
+		break;
+	}
+	case ESectionState::COMPLETE:
+	{
+		Trigger->SetCollisionProfileName(TEXT("NoCollision"));
+		for (UBoxComponent* GateTrigger : GateTriggers)
+		{
+			GateTrigger->SetCollisionProfileName(TEXT("ABTrigger"));
+		}
+
+		OperateGates(true);
+		break;
+	}
+	}
+
+	CurrentState = NewState;
+}
+
+void AABSection::OperateGates(bool bOpen)
+{
+	for (UStaticMeshComponent* Gate : GateMeshes)
+	{
+		Gate->SetRelativeRotation(bOpen ? FRotator(0.0f, -90.0f, 0.0f) : FRotator::ZeroRotator);
+	}
+}
+
+void AABSection::OnTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (CurrentState == ESectionState::READY)
+	{
+		SetState(ESectionState::BATTLE);
+	}
+}
+
+void AABSection::OnGateTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	ABCHECK(OverlappedComponent->ComponentTags.Num() == 1);
+
+	FName ComponentTag = OverlappedComponent->ComponentTags[0];
+	FName SocketName = FName(*ComponentTag.ToString().Left(2));
+	if (!Mesh->DoesSocketExist(SocketName))
+		return;
+
+	FVector NewLocation = Mesh->GetSocketLocation(SocketName);
+
+	TArray<FOverlapResult> OverlapResults;
+	FCollisionQueryParams CollisionQueryParam(NAME_None, false, this);
+	FCollisionObjectQueryParams ObjectQueryParam(FCollisionObjectQueryParams::InitType::AllObjects);
+	bool bResult = GetWorld()->OverlapMultiByObjectType(
+		OverlapResults,
+		NewLocation,
+		FQuat::Identity,
+		ObjectQueryParam,
+		FCollisionShape::MakeSphere(775.0f),
+		CollisionQueryParam
+	);
+
+	if (!bResult)
+	{
+		auto NewSection = GetWorld()->SpawnActor<AABSection>(NewLocation, FRotator::ZeroRotator);
+	}
+	else
+	{
+		ABLOG(Warning, TEXT("New section area is not empty."));
+	}
 }
--- Source/ArenaBattle/Public/ABSection.h
+++ Source/ArenaBattle/Public/ABSection.h
@@ -14,16 +14,44 @@ class ARENABATTLE_API AABSection : public AActor
 public:	
 	// Sets default values for this actor's properties
 	AABSection();
+	virtual void OnConstruction(const FTransform& Transform) override;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 private:
+	enum class ESectionState : uint8
+	{
+		READY = 0,
+		BATTLE,
+		COMPLETE
+	};
+
+	void SetState(ESectionState NewState);
+	ESectionState CurrentState = ESectionState::READY;
+
+	void OperateGates(bool bOpen = true);
+
+	UFUNCTION()
+	void OnTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnGateTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+private:
 	UPROPERTY(VisibleAnywhere, Category = Mesh, Meta = (AllowPrivateAccess = true))
 	TArray<UStaticMeshComponent*> GateMeshes;
+
+	UPROPERTY(VisibleAnywhere, Category = Trigger, Meta = (AllowPrivateAccess = true))
+	TArray<UBoxComponent*> GateTriggers;
 
 	UPROPERTY(VisibleAnywhere, Category = Mesh, Meta = (AllowPrivateAccess = true))
 	UStaticMeshComponent* Mesh;
 
+	UPROPERTY(VisibleAnywhere, Category = Trigger, Meta = (AllowPrivateAccess = true))
+	UBoxComponent* Trigger;
+
+	UPROPERTY(EditAnywhere, Category = State, Meta = (AllowPrivateAccess = true))
+	bool bNoBattle;
 };
